6535d06e8639ed4581f62945964cfd59e4f02bef
Alexander Stoklasa <astoklas@gmail.com>
2015-12-06 13:15:21 +0100

    Assignment 11

---
+++ Coursera/Python Access Web Data/socket1.py
@@ -0,0 +1,22 @@
+import socket
+import urllib
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.pythonlearn.com', 80))
+mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
+
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print data;
+
+mysock.close()
+
+
+fhand = urllib.urlopen('http://www.pythonlearn.com/code/intro-short.txt')
+print "Return Code: ",fhand.getcode()
+print "", fhand.headers()
+for line in fhand:
+    print line.strip()
+
